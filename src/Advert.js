8878588b494599c0a95dcad1967e9e2bc3f1b4b0
bgrego11 <makhno@makhnos-MacBook-Pro.local>
2018-04-01 17:52:49 -0400

    advert rendering fine. Still touching up some css

--- src/Advert.js
+++ src/Advert.js
@@ -9,43 +9,70 @@ import styles from './Advert.module.scss';
 //modules
 
 
+
+
 class Advert extends Component {
+    getInitialState(){
+        return {
+            hover: false
+                }
+        }
+      
+
+    toggleHover(){
+        this.setState({hover: !this.state.hover})
+      }
+
   render() {
     return (
         <div>
             <h3>What We Offer</h3>
-            <div className={styles.adContainer}>
-                <div className={styles.adMain}>
-                <div>
-                        <i className="fas fa-cloud fa-7x"></i>
-                    </div>
-                    <div className={styles.adTitle}>
-                        Web Design and Development
-                    </div>
-                </div>
-                <div className={styles.adMain}>
-                <div>
-                        <i className="fas fa-puzzle-piece fa-7x"></i>
-                    </div>
-                    <div className={styles.adTitle}>
-                    
-                        Integration
-                    </div>
-                </div>
-                <div className={styles.adMain}>
-                    <div>
-                        <i className="fas fa-mobile fa-7x"></i>
-                    </div>
-                    <div className={styles.adTitle}>
-                        Mobile Solutions
-                    </div>
-                </div>
-            </div>
-
+            <AdPane title="Web Design and Development" content="From conception and strategy to design and implementation, Americaneagle.com builds and hosts premium websites for businesses, associations, government, and Fortune 500 companies" icon="fas fa-cloud fa-7x"></AdPane>
+            <AdPane title="Integration" content="Piranha Technologies' integrations team has the experience and expertise to seamlessly integrate any third party software that your organization utilizes in order to give your users the best online experience possible." icon="fas fa-puzzle-piece fa-7x"></AdPane>
+            <AdPane title="Mobile Solutions" content="With the influx of smartphones, tablets, and everything in between, it's quickly become mandatory that your business is accessible on the go via a mobile site or app." icon="fas fa-mobile fa-7x"></AdPane>
         </div>
 
     );
   }
 }
+
+class AdPane extends Component {
+    
+    constructor(props) {
+        super(props)
+        this.state = {hover: false}
+        this.toggleHover = this.toggleHover.bind(this)
+      }
+
+    toggleHover(){
+        this.setState({hover: !this.state.hover})
+      }
+    
+
+  render() {
+    return (
+            
+            <div className={styles.adContainer} onMouseEnter={this.toggleHover}
+            onMouseLeave={this.toggleHover}>
+                <div className={styles.adMain}>
+                    <div className={styles.fas}>
+                        <i className={this.props.icon}></i>
+                    </div>
+                    <div className={styles.adTitle}>
+                        {this.props.title}
+                    </div>
+                    {this.state.hover &&
+                    <div className={styles.adContent}>
+                        {this.props.content}
+                    </div>
+                    }
+                </div>
+            </div>
+
+    );
+  }
+}
+
+
 
 export default Advert;
